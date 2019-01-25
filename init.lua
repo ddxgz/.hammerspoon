64e7abd04dba6e9c63a1f8da4923b11bdb206121
Cong Peng <ddxgz19880813@gmail.com>
2019-01-25 17:15:39 +0100

    add Hammerspoon's find the mouse point

--- init.lua
+++ init.lua
@@ -23,6 +23,37 @@ spoon.Caffeine:start()
 
 
 ---------------------------------------------------------
+-- find the mouse point - from hammperspoon getting started
+---------------------------------------------------------
+mouseCircle = nil
+mouseCircleTimer = nil
+
+function mouseHighlight()
+  -- Delete an existing highlight if it exists
+  if mouseCircle then
+    mouseCircle:delete()
+    if mouseCircleTimer then
+      mouseCircleTimer:stop()
+    end
+  end
+  -- Get the current co-ordinates of the mouse pointer
+  mousepoint = hs.mouse.getAbsolutePosition()
+  -- Prepare a big red circle around the mouse pointer
+  mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+  mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+  mouseCircle:setFill(false)
+  mouseCircle:setStrokeWidth(9)
+  mouseCircle:show()
+
+  -- Set a timer to delete the circle after 3 seconds
+  mouseCircleTimer = hs.timer.doAfter(3, function() mouseCircle:delete() end)
+end
+hs.hotkey.bind({"cmd","alt","shift"}, "D", mouseHighlight)
+
+---------------------------------------------------------
+
+
+---------------------------------------------------------
 -- window management
 ---------------------------------------------------------
 -- https://github.com/miromannino/miro-windows-manager
@@ -42,7 +73,7 @@ spoon.MiroWindowsManager:bindHotkeys({
 })
 
 
--- move window to another screen in the cycle
+-- move window to the next screen in the cycle
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
     local win = hs.window.focusedWindow()
     local curScr = win:screen()
